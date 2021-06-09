9bf61d0da7a84ee5225014711bf04778a99c1d2a
Stef Kuypers <stef@happonomy.org>
2021-06-09 16:34:20 +0200

    Added random growth functionality

--- src/Simulation.jl
+++ src/Simulation.jl
@@ -2,10 +2,21 @@ using EconoSim # EconoSim 0.1.2 or https://github.com/HapponomyOrg/EconoSim.jl
 using Plots
 using CSV
 
-@enum Mode fixed_growth fixed_loan_rate
+"""
+    Mode in which the simulation is run.
+* fixed_growth: growth ratio and profit ratio is fixed.
+* random_growth: growth ratio is random, profit ratio is relative to growth ratio.
+* fixed loan ratio: loan ratio and profit ratio is fixed.
+"""
+@enum Mode fixed_growth random_growth fixed_loan_ratio
+
+RealFunc = Union{Real, Function}
+
+value(rf::RealFunc) = rf isa Function ? rf() : rf
 
 struct SimData
-    growth::Vector{Fixed(4)}
+    growth_ratio::Vector{Fixed(4)}
+    profit_ratio::Vector{Fixed(4)}
     qe_adjustment::Vector{Currency}
     available::Vector{Currency}
     saved::Vector{Currency}
@@ -15,6 +26,7 @@ struct SimData
     loan::Vector{Currency}
     bank_equity::Vector{Currency}
     SimData(size::Integer) = new(Vector{Fixed(4)}(undef, size),
+                                Vector{Fixed(4)}(undef, size),
                                 Vector{Currency}(undef, size),
                                 Vector{Currency}(undef, size),
                                 Vector{Currency}(undef, size),
@@ -25,7 +37,7 @@ struct SimData
                                 Vector{Currency}(undef, size))
 end
 
-Base.length(data::SimData) = length(data.growth)
+Base.length(data::SimData) = length(data.growth_ratio)
 
 # agregate bank balance sheet
 bank = Balance(def_min_asset = typemin(Currency), log_transactions = false)
@@ -41,7 +53,8 @@ current_money_stock() = current_available() + current_saved()
 money_stock(data::SimData) = .+(data.available, data.saved)
 debt_ratio(data::SimData) = ./(100 * data.debt, money_stock(data))
 required_loan_ratio(data::SimData) = ./(100 * data.required_loan, money_stock(data))
-growth(data::SimData) = .*(data.growth, 100)
+growth_ratio(data::SimData) = .*(data.growth_ratio, 100)
+profit_ratio(data::SimData) = .*(data.profit_ratio, 100)
 
 function Base.resize!(data::SimData, size::Integer)
     if size != length(data)
@@ -56,13 +69,14 @@ end
 function process_cycle!(cycle::Integer,
                     data::SimData,
                     mode::Mode,
-                    mode_ratio::Real,
+                    mode_ratio::RealFunc,
                     loans::Vector{Debt},
-                    bank_profit_rate::Real,
+                    profit_ratio::Real,
                     save_ratio::Real,
                     maturity::Integer,
                     loan_satisfaction::Real)
     money_stock = current_money_stock()
+    mode_ratio = value(mode_ratio)
 
     # process loans
     for loan in loans
@@ -77,9 +91,17 @@ function process_cycle!(cycle::Integer,
     end
 
     # determine required and real loan
-    if mode == fixed_growth
+    if mode != fixed_loan_ratio
         target_money_stock = (1 + mode_ratio) * money_stock
         data.required_loan[cycle] = max(0, target_money_stock - current_money_stock())
+
+        if mode == random_growth
+            if cycle == 1
+                profit_ratio = 0
+            else
+                profit_ratio = mode_ratio + profit_ratio
+            end
+        end
     else
         data.required_loan[cycle] = max(0, (money_stock - data.installment[cycle]) / (1 - mode_ratio) * mode_ratio)
     end
@@ -87,13 +109,14 @@ function process_cycle!(cycle::Integer,
     data.loan[cycle] = data.required_loan[cycle] * loan_satisfaction
 
     if data.loan[cycle] > 0
-        push!(loans, bank_loan(bank, available, data.loan[cycle], bank_profit_rate, maturity))
+        push!(loans, bank_loan(bank, available, data.loan[cycle], profit_ratio, maturity))
     end
 
     # save money if available
     transfer_asset!(available, saved, DEPOSIT, max(0, current_available() * save_ratio))
 
-    data.growth[cycle] = Fixed(4)((current_money_stock() - money_stock)) / money_stock
+    data.growth_ratio[cycle] = Fixed(4)((current_money_stock() - money_stock)) / money_stock
+    data.profit_ratio[cycle] = mode_ratio
     data.available[cycle] = current_available()
     data.saved[cycle] = current_saved()
     data.debt[cycle] = asset_value(bank, DEBT)
@@ -102,10 +125,10 @@ end
 
 """
     simulate_banks(mode::Mode,
-                    mode_ratio::Real,
+                    mode_ratio::RealFunc,
                     money_stock::Real,
                     debt_ratio::Real,
-                    bank_profit_rate::Real,
+                    bank_profit_rate::RealFunc,
                     save_ratio::Real,
                     maturity::Integer,
                     cycles::Integer;
@@ -113,20 +136,20 @@ end
 
 # Arguments
 * mode::Mode - The simulation mode.
-* mode_ratio::Real - Either the growth factor of the money stock or the koan rate per cycle.
+* mode_ratio::RealFunc - Either the growth factor of the money stock or the loan rate per cycle.
 * money_stock::Real - The initial money stock.
 * debt_ratio::Real - The initial debt ratio.
-* bank_profit_rate::Real - The net bank profit rate on bank debt per cycle.
+* profit_ratio::Real - The net bank profit rate on bank debt per cycle.
 * save_ratio::Real - The percentage of money stock which is saved each cycle. This money is not available to pay off debt with. Saved money is part of the money stock.
 * maturity::Integer - The maturity of the loans.
 * cycles::Integer - The number of cycles the simulation runs.
 * loan_satisfaction::Real - The satisfaction rate of the required loan.
 """
 function simulate_banks(mode::Mode,
-                mode_ratio::Real,
+                mode_ratio::RealFunc,
                 money_stock::Real,
                 debt_ratio::Real,
-                bank_profit_rate::Real,
+                profit_ratio::Real,
                 save_ratio::Real,
                 maturity::Integer,
                 cycles::Integer;
@@ -140,7 +163,12 @@ function simulate_banks(mode::Mode,
     clear!(saved)
 
     loans = Vector{Debt}()
-    push!(loans, bank_loan(bank, available, debt, bank_profit_rate, maturity)) # create initial debt
+
+    if mode == random_growth
+        push!(loans, bank_loan(bank, available, debt, 0, maturity)) # set profit ratio for initial debt to 0
+    else
+        push!(loans, bank_loan(bank, available, debt, 0, maturity)) # create initial debt
+    end
 
     # Adjust for debt ratio's different from 100%
     book_asset!(available, DEPOSIT, money_stock - debt)
@@ -157,7 +185,7 @@ function simulate_banks(mode::Mode,
                         mode,
                         mode_ratio,
                         loans,
-                        bank_profit_rate,
+                        profit_ratio,
                         save_ratio,
                         maturity,
                         loan_satisfaction)
@@ -169,40 +197,6 @@ function simulate_banks(mode::Mode,
     end
 
     return resize!(data, num_cycles)
-end
-
-function process_data(money_stock::Vector{Real},
-                    debt::Vector{Real},
-                    savings::Vector{Real},
-                    qe::Vector{Real})
-    money_stock = Vector{Currency}(money_stock)
-    debt = Vector{Currency}(debt)
-    savings = Vector{Currency}(savings)
-    qe = Vector{Currency}(qe)
-
-    data = SimData(length(money_stock))
-    data.saved[1] = savings[1]
-    data.available[1] = money_stock[1] - savings[1]
-    data.debt[1] = debt[1]
-    data.qe_adjustment[1] = qe[1]
-    data.bank_equity[1] = debt[1] - money_stock[1]
-
-    for cycle in 2:length(money_stock)
-        data.growth[cycle] = (money_stock[cycle] - money_stock[cycle - 1]) / money_stock[cycle - 1]
-        data.saved[cycle] = savings[cycle]
-        data.available[cycle] = money_stock[cycle] - savings[cycle]
-        data.debt[cycle] = debt[cycle]
-        data.qe_adjustment[cycle] = data.qe_adjustment[cycle - 1] + qe[cycle]
-        data.loan[cycle] = debt[cycle] - debt[cycle - 1]
-        data.bank_equity[cycle] = debt[cycle] - money_stock[cycle]
-        data.bank_profit_rate[cycle] = (data.bank_equity[cycle] - data.bank_equity[cycle - 1]) / (data.bank_equity[cycle - 1])
-    end
-
-    data.growth[1] = data.growth[2]
-    data.loan[1] = data.loan[2]
-    data.bank_profit_rate[1] = data.bank_profit_rate[2]
-
-    return data
 end
 
 function plot_required_loan_ratio(data::SimData)
@@ -221,9 +215,17 @@ function plot_debt_ratio(data::SimData)
     return yaxis!("Percentage")
 end
 
-function plot_growth(data::SimData)
-    series = growth(data)
-    plot(series, label = "g", title = "Growth (" * string(length(data)) * " years)")
+function plot_growth_ratio(data::SimData)
+    series = growth_ratio(data)
+    plot(series, label = "g", title = "Growth ratio (" * string(length(data)) * " years)")
+    xaxis!("Years")
+
+    return yaxis!("Percentage")
+end
+
+function plot_profit_ratio(data::SimData)
+    series = profit_ratio(data)
+    plot(series, label = "p", title = "Profit ratio (" * string(length(data)) * " years)")
     xaxis!("Years")
 
     return yaxis!("Percentage")
@@ -237,29 +239,38 @@ function plot_money_stock(data::SimData)
     return yaxis!("Stock")
 end
 
-function plot_data(mode::Mode, m::Real, p::Real, s::Real, maturity::Integer, cycles::Integer = 100)
+function plot_data(mode::Mode, m::RealFunc, p::Real, s::Real, maturity::Integer, cycles::Integer = 100)
     data = simulate_banks(mode, m, 1000000, 1, p, s, maturity, cycles)
-    m = Fixed(4)(m * 100)
+    m = m isa Function ? m : Fixed(4)(m * 100)
     p = Fixed(4)(p * 100)
     s = Fixed(4)(s * 100)
 
-    if mode == fixed_growth
+    if mode != fixed_loan_ratio
         plot_required_loan_ratio(data)
         savefig("plots/" * string(mode) * "_LR_growth_" * string(m) * "_profit_" * string(p) * "_save_" * string(s) * "_" * string(maturity) * "_" * string(cycles) * ".png")
         plot_debt_ratio(data)
         savefig("plots/" * string(mode) * "_DR_growth_" * string(m) * "_profit_" * string(p) * "_save_" * string(s) * "_" * string(maturity) * "_" * string(cycles) * ".png")
 
+        if mode == random_growth
+            plot_growth_ratio(data)
+            savefig("plots/" * string(mode) * "_g_growth_" * string(m) * "_profit_" * string(p) * "_save_" * string(s) * "_" * string(maturity) * "_" * string(cycles) * ".png")
+        end
+
         return last(required_loan_ratio(data)), last(debt_ratio(data))
     else
-        plot_growth(data)
+        plot_growth_ratio(data)
         savefig("plots/" * string(mode) * "_g_LR_" * string(m) * "_profit_" * string(p) * "_save_" * string(s) * "_" * string(maturity) * "_" * string(cycles) * ".png")
         plot_money_stock(data)
-        savefig("plots/" * string(mode, false) * "_M_LR_" * string(m) * "_profit_" * string(p) * "_save_" * string(s) * "_" * string(maturity) * "_" * string(cycles) * ".png")
+        savefig("plots/" * string(mode) * "_M_LR_" * string(m) * "_profit_" * string(p) * "_save_" * string(s) * "_" * string(maturity) * "_" * string(cycles) * ".png")
         plot_debt_ratio(data)
         savefig("plots/" * string(mode) * "_DR_LR_" * string(m) * "_profit_" * string(p) * "_save_" * string(s) * "_" * string(maturity) * "_" * string(cycles) * ".png")
 
-        return last(growth(data)), last(debt_ratio(data))
+        return last(growth_ratio(data)), last(debt_ratio(data))
     end
+end
+
+function get_random_growth(low, high)
+    return rand() * (high - low) + low
 end
 
 function simulations()
@@ -295,6 +306,14 @@ function simulations()
         plot_data(fixed_loan_rate, 0.15, 0.013, 0, maturity, 500)
         plot_data(fixed_loan_rate, 0.15, 0.02, 0, maturity, 500)
     end
+end
+
+function random_simulations()
+    grow_func() = get_random_growth(0.02, 0.12)
+
+    plot_data(random_growth, grow_func, -0.05, 0, 20, 500)
+    plot_data(random_growth, grow_func, 0, 0, 20, 500)
+    plot_data(random_growth, grow_func, 0.05, 0, 20, 500)
 end
 
 function process_csv(csv_file::String)
