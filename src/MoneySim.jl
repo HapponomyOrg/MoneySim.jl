74f381e112c804fba2e1f7c4e065497aa7a81f1e
Stef Kuypers <stef@happonomy.org>
2021-06-12 14:01:58 +0200

    Updated exports

--- src/MoneySim.jl
+++ src/MoneySim.jl
@@ -2,7 +2,8 @@ module MoneySim
 
 include("Simulation.jl")
 
-export SimData, RealFunc, IntFunc, Mode
+export Mode, growth_mode, loan_ratio_mode
+export SimData, RealFunc, IntFunc
 export simulate_banks, plot_data, simulations
 export plot_maturity, plot_loan_ratio, plot_debt_ratio, plot_growth_ratio, plot_profit_ratio, plot_delta_growth_profit, plot_money_stock, plot_D_over_L
 export random_float, random_int
