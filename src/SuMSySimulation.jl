ac70ee4d64d75f0e155d744f20457d8d74340be7
Stef Kuypers <stef@happonomy.org>
2022-06-18 11:33:44 +0200

    Updated

--- src/SuMSySimulation.jl
+++ src/SuMSySimulation.jl
@@ -1,4 +1,5 @@
 using EconoSim
+using Plots
 using StatsPlots
 using Random
 using Agents
@@ -6,23 +7,14 @@ using DataFrames
 
 NUM_ACTORS = 1000
 
-BOTTOM_0_1 = 1:Int64(round(NUM_ACTORS / 1000))
-BOTTOM_1 = 1:Int64(NUM_ACTORS / 100)
-BOTTOM_10 = 1:Int64(NUM_ACTORS / 10)
-BOTTOM_50 = 1:Int64(NUM_ACTORS / 2)
-MIDDLE_40 = Int64(NUM_ACTORS / 2 + 1):Int64(NUM_ACTORS - NUM_ACTORS / 10)
-TOP_10 = Int64(NUM_ACTORS - NUM_ACTORS / 10 + 1):NUM_ACTORS
-TOP_1 = Int64(NUM_ACTORS - NUM_ACTORS / 100 + 1):NUM_ACTORS
-TOP_0_1 = Int64(round(NUM_ACTORS + 1 - NUM_ACTORS / 1000)):NUM_ACTORS
-PERCENTILES = [BOTTOM_0_1, BOTTOM_1, BOTTOM_10, BOTTOM_50, MIDDLE_40, TOP_10, TOP_1, TOP_0_1]
-
 SIM_LENGTH = 1000
 NUM_TRANSACTIONS = 200
 
 BASIC_SUMSY = SuMSy(2000, 0, 0.01, 30)
 DEM_FREE_SUMSY = SuMSy(2000, 25000, 0.01, 30)
-TIERED_SUMSY = SuMSy(2000, 25000, [(0, 0.01), (50000, 0.02), (500000, 0.05), (1000000, 0.1), (10000000, 0.5), (50000000, 0.95)], 30)
-BROKEN_SUMSY = SuMSy(2000, 0, 0, 30)
+TIERED_SUMSY = SuMSy(2000, 0, [(0, 0.01), (50000, 0.02), (500000, 0.05), (1000000, 0.1), (10000000, 0.5), (50000000, 0.95)], 30)
+TIERED_DEM_FREE_SUMSY = SuMSy(2000, 25000, [(0, 0.01), (50000, 0.02), (500000, 0.05), (1000000, 0.1), (10000000, 0.5), (50000000, 0.95)], 30)
+NO_DEM_SUMSY = SuMSy(2000, 0, 0, 30)
 NO_SUMSY = SuMSy(0, 0, 0, 30)
 NO_GI_SUMSY = SuMSy(0, 0, 0.01, 30)
 
@@ -36,35 +28,41 @@ end
 
 ### Behaviors
 
+function yard_sale_transfer!(target1, target2)
+    av1 = max(CUR_0, asset_value(target1.balance, SUMSY_DEP))
+    av2 = max(CUR_0, asset_value(target2.balance, SUMSY_DEP))
+    transfer = max(C_0,
+            Currency(min(av1, av2) * C_0_2))
+
+    if rand(1:2) == 1
+        source = target1.balance
+        destination = target2.balance
+        avs = av1
+        avd = av2
+    else
+        source = target2.balance
+        destination = target1.balance
+        avs = av2
+        avd = av1
+    end
+    
+    # Make sure a minimum amount is always transferred
+    if transfer < C_20 &&
+         avs > 0 &&
+         avs > avd
+         transfer = min(C_20, avs * C_0_2)
+    end
+
+    
+    transfer_asset!(source, destination, SUMSY_DEP, transfer)
+end
+
 function yard_sale!(model)
+    target1 = random_agent(model)
+    target2 = random_agent(model)
+
     for i in 1:model.num_transactions
-        target1 = random_agent(model)
-        target2 = random_agent(model)
-        av1 = max(CUR_0, asset_value(target1.balance, SUMSY_DEP))
-        av2 = max(CUR_0, asset_value(target2.balance, SUMSY_DEP))
-        transfer = max(C_0,
-                Currency(min(av1, av2) * C_0_2))
-
-        if rand(1:2) == 1
-            source = target1.balance
-            destination = target2.balance
-            avs = av1
-            avd = av2
-        else
-            source = target2.balance
-            destination = target1.balance
-            avs = av2
-            avd = av1
-        end
-        
-        # Make sure a minimum amount is always transferred
-        if transfer < C_20 &&
-             avs > 0 &&
-             avs > avd
-             transfer = min(C_20, avs * C_0_2)
-        end
-
-        transfer_asset!(source, destination, SUMSY_DEP, transfer)
+        yard_sale_transfer!(target1, target2)
     end
 end
 
@@ -188,59 +186,6 @@ function constant_money_destruction!(model)
     end
 end
 
-function borrow_income(model)
-    sumsy = model.sumsy_base
-    step = model.step
-
-    if process_ready(sumsy, step)
-        for actor in allagents(model)
-            settled_debts = []
-
-            for debt in actor.debts
-                if debt_settled(process_debt!(debt))
-                    push!(settled_debts, debt)
-                end
-            end
-
-            for debt in settled_debts
-                delete_element!(actor.debts, debt)
-            end
-
-            income = max(0, sumsy.guaranteed_income - calculate_demurrage(actor.balance, sumsy, step))
-
-            if (income > 0)
-                push!(actor.debts, bank_loan(model.bank, actor.balance, income, 0, 20, sumsy.interval, step, money_entry = SUMSY_DEP))
-            end
-        end
-    end
-end
-
-function run_debt_based_simulation(sumsy_base::SuMSy = BASIC_SUMSY;
-                                    active_sumsy = NO_SUMSY,
-                                    num_actors::Integer = NUM_ACTORS,
-                                    num_transactions::Integer = NUM_TRANSACTIONS,
-                                    sim_length::Integer = SIM_LENGTH,
-                                    concentrated::Bool = false)
-    set_random_seed()
-    model = create_sumsy_model(active_sumsy, borrow_income)
-    model.properties[:sumsy_base] = sumsy_base
-    model.properties[:bank] = Balance()    
-    start_balance = telo(sumsy_base)
-    model.properties[:num_transactions] = num_transactions
-
-    add_actors!(model, num_actors, start_balance, SUMSY_DEP, concentrated = concentrated)
-
-    for actor in allagents(model)
-        actor.debts = []
-    end
-
-    add_model_behavior!(model, yard_sale!)
-
-    data, _ = run_econo_model!(model, sim_length, adata = [deposit_data, equity_data])
-
-    return data
-end
-
 function analyse_money_stock(data)
     # deleteat!(data, 1:NUM_ACTORS)
     groups = groupby(data, :step)
@@ -273,26 +218,44 @@ function plot_money_stock(dataframe, title::String = ""; theoretical_min = nothi
     end
 end
 
+function plot_comparative_money_stock(dataframe,
+                                        title::String = "";
+                                        original_min = nothing,
+                                        original_max = nothing,
+                                        new_min = nothing,
+                                        new_max = nothing)
+    @df dataframe plot(:money_stock, title = "Money stock\n" * title, label = "new money stock", color = "blue")
+
+    if original_min !== nothing
+        plot!(fill(original_min, size(dataframe[!, :money_stock])[1]), label = "original min", color = "orange")
+    end
+
+    if original_max !== nothing
+        plot!(fill(original_max, size(dataframe[!, :money_stock])[1]), label = "original max", color = "red")
+    end
+
+    if new_min !== nothing
+        plot!(fill(new_min, size(dataframe[!, :money_stock])[1]), label = "new min", color = "green")
+    end
+
+    if new_max !== nothing
+        plot!(fill(new_max, size(dataframe[!, :money_stock])[1]), label = "new max", color = "purple")
+    end
+end
+
 function plot_money_stock_per_person(dataframe, num_actors = NUM_ACTORS, title::String = "")
     plot(./(dataframe[!, :money_stock], num_actors), title = "Money stock\n" * title, label = "Money stock")
 end
 
-function time_telo(sumsy::SuMSy, balance::Balance = Balance())
-    t = 0
-    eq = telo(sumsy)
+function plot_net_incomes(sumsy::SuMSy)
+    b = Balance()
+    incomes = Vector{Currency}()
 
-    while asset_value(balance, SUMSY_DEP) < eq - 1
-        dem = calculate_demurrage(balance, sumsy, 0)
-        book_asset!(balance, SUMSY_DEP, sumsy.guaranteed_income - dem)
-        t += 1
+    for i in 0:10:telo(sumsy)
+        append!(incomes,
+                sumsy.guaranteed_income
+                - calculate_demurrage(i, sumsy))
     end
 
-    return t
-end
-
-function plot_balance(sumsy::SuMSy, income::Currency = CUR_0)
-    c = 1 - sumsy.dem_tiers
-    balances = []
-
-
+    plot(incomes, label = "net income", xlabel="Acount balance")
 end
