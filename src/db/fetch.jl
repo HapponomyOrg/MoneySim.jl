51770534930a352bb7143d475af078f4a2153472
Stef Kuypers <stef@happonomy.org>
2025-10-27 15:34:32 +0100

    Init

---
+++ src/db/fetch.jl
@@ -0,0 +1,29 @@
+# DB fetch functions
+
+function fetch_money_data(country)
+    # M1, M2 and M3
+end
+
+function fetch_inequality_data(country)
+    # from World Inequality DB
+    # Top 0.001%
+    # Top 0.01%
+    # Top 0.1%
+    # Top 1%
+    # Top 10%
+    # Middle 40% (high)
+    # Middle 40% (low)
+    # Bottom 10%
+end
+
+function fetch_demographic_data(country)
+    # Population -18
+    # Population 18 to 65
+    # Population 65+
+end
+
+function fetch_government_expenses(country)
+end
+
+function fetch_government_income(country)
+end
